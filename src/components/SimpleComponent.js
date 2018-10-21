8955005553e2dd7aa2f37206084cedd0874a9c5e
Sharmina Akhtar <sakht318@gmail.com>
2018-10-21 18:56:30 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,18 @@
 // Code SimpleComponent Here
+
+
+class SimpleComponent extends Component {
+  state = { mood: "happy" };
+  handleClick = (event) => {
+    this.setState: {
+      mood: "sad"
+    }
+  }
+  render() {
+    return (
+      <div>
+      <button onClick={this.handleClick}>{mood}</button>
+      </div>
+    );
+  }
+}
